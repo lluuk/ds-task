43a28841939972d71963961b1dd8c4e42d40241c
Łukasz Zapolski <lukasz.zapolski@booksy.com>
2021-07-07 23:44:28 +0200

    chore: routing adjustments

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <el-button>adssasa</el-button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+import { ElButton } from "element-plus";
 
 export default defineComponent({
   name: "Home",
   components: {
     HelloWorld,
+    ElButton,
   },
 });
 </script>
