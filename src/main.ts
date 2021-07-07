dcd28d241797012e7a6d2c48327ee62f47fd6509
Łukasz Zapolski <lukasz.zapolski@booksy.com>
2021-07-07 23:43:31 +0200

    chore: setup stylessheets and ui lib

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "element-plus/packages/theme-chalk/src/base.scss";
+import "@/assets/stylesheets/stylesheets.scss";
 
 createApp(App).use(router).mount("#app");
