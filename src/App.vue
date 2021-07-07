aba2cae2f49221c4f57bbba8da962e5fac6cec32
Łukasz Zapolski <lukasz.zapolski@booksy.com>
2021-07-07 23:45:25 +0200

    feat: provided init version of layout components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <el-container>
+    <the-header />
+    <el-main>
+      <router-view />
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import { ElContainer, ElMain, ElFooter } from "element-plus";
+import TheHeader from "@/components/layout/TheHeader.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    ElContainer,
+    TheHeader,
+    ElMain,
+    ElFooter,
+  },
+});
+</script>
 
 <style lang="scss">
 #app {
@@ -13,18 +32,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,78 @@
+<template>
+  <el-header class="header">
+    <img alt="Vue logo" src="@/assets/logo.png" class="logo" />
+    <div>
+      <router-link
+        v-for="link in links"
+        :to="link.path"
+        :key="link.path"
+        class="link"
+      >
+        {{ link.name }}
+      </router-link>
+    </div>
+  </el-header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ElHeader } from "element-plus";
+
+interface Link {
+  name: string;
+  path: string;
+}
+
+export default defineComponent({
+  name: "TheHeader",
+  components: {
+    ElHeader,
+  },
+
+  setup() {
+    const links: Link[] = [
+      {
+        name: "User Form",
+        path: "/",
+      },
+      {
+        name: "User Profile",
+        path: "/profile",
+      },
+    ];
+
+    return {
+      links,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: #f2f6fc;
+}
+
+.logo {
+  height: 100%;
+  max-height: 50px;
+  width: auto;
+}
+
+.link {
+  font-weight: 500;
+  color: $color-primary;
+  text-decoration: none;
+
+  &:hover {
+    color: $color-blue;
+  }
+
+  &:not(:last-of-type) {
+    margin-right: 15px;
+  }
+}
+</style>
